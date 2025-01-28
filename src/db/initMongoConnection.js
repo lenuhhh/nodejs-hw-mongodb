77f786c0ee05d41d423ab65a1958a037aa8da6a4
lenuhhh <161879235+lenuhhh@users.noreply.github.com>
2025-01-29 00:16:06 +0200

    Update initMongoConnection.js

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -5,15 +5,6 @@ import { getEnvVar } from "../utils/getEnvVar.js"
 
 export const initMongoConnection = async () => {
     try {
-
-        const user = getEnvVar('MONGODB_USER')
-
-        const password = getEnvVar('MONGODB_PASSWORD')
-
-        const url = getEnvVar('MONGODB_URL')
-
-        const db = getEnvVar('MONGODB_DB')
-
         await mongoose.connect(`mongodb+srv://alexander:82NXfZsmMD6yL5Ms@cluster0.46t38.mongodb.net/contacts?retryWrites=true&w=majority&appName=Cluster0`)
 
         console.log('Mongo connection successfully established')   
