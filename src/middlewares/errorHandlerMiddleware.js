96964a96a6458919a63a2031a4cd58194c6dbdc9
lenuhhh <161879235+lenuhhh@users.noreply.github.com>
2025-02-09 19:50:18 +0200

    Update errorHandlerMiddleware.js

--- src/middlewares/errorHandlerMiddleware.js
+++ src/middlewares/errorHandlerMiddleware.js
@@ -1,15 +1,14 @@
-import { HttpError } from 'http-errors';
-
-
+import { isHttpError } from 'http-errors'
+import { MongooseError } from 'mongoose'
 
 export const errorHandlerMiddleware = (e, req, res, next) => {
-    if (e instanceof HttpError) {
-
-    res.status(e.status).json({ status: e.status, message: e.name, data: e,
-    });
-
-    return;
+    if (isHttpError(e)) {
+    return res.status(e.status).json({ status: e.status, message: e.name, data: e })
   }
-    res.status(500).json({ message: 'Something went wrong', error: e.message
-    })
+
+  if (e instanceof MongooseError) {
+    return res.status(e.status).json({ status: e.status, message: 'Mongoose error', data: { message: e.message }} )
+  }
+
+  res.status(500).json({ status: 500, message: 'Something went wrong', error: e.message })
 }
